235e8467377e42bac998bfcd048b43fb259c65fd
Aditya Nikumbh <adi@wirelessprv-10-193-77-40.near.illinois.edu>
2022-12-01 01:14:32 -0600

    cleaned up code

--- Day1/day1solution.py
+++ Day1/day1solution.py
@@ -1,19 +1,8 @@
-
-text = open("day1.txt")
-text = text.read()
-
-split_text = text.split("\n\n")
-all = [ ]
-for i in range(len(split_text)):
-    resplit_text = split_text[i].split("\n")
-    for j in range(len(resplit_text)):
-        resplit_text[j] = int(resplit_text[j]) 
-    potentialGreatest = sum(resplit_text)
-    all.append(potentialGreatest)
-
-print(max(all))
-all.sort()
-all = all[::-1]
-print(all)
-top3 = all[0:3]
-print(sum(top3))
+inputFile = (open("day1.txt")).read()
+splitPerElf = inputFile.split("\n\n")
+sumPerElf = []
+for i in range(len(splitPerElf)):
+    foodPerElf = sumPerElf[i].split("\n")
+    sumPerElf.append(sum([int(x) for x in foodPerElf]))
+sumPerElf.sort(reverse=True)
+print("Part 1:", sumPerElf[0], " \nPart 2:", sum(sumPerElf[0:2]))
