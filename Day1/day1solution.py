73b12f31305c2b0c39bbe66080213610ed8de684
Aditya Nikumbh <adi@wirelessprv-10-193-77-40.near.illinois.edu>
2022-12-01 01:23:16 -0600

    cleaned up code

--- Day1/day1solution.py
+++ Day1/day1solution.py
@@ -1,8 +1,8 @@
-inputFile = (open("day1.txt")).read()
-splitPerElf = inputFile.split("\n\n")
-sumPerElf = []
-for i in range(len(splitPerElf)):
-    foodPerElf = sumPerElf[i].split("\n")
-    sumPerElf.append(sum([int(x) for x in foodPerElf]))
-sumPerElf.sort(reverse=True)
-print("Part 1:", sumPerElf[0], " \nPart 2:", sum(sumPerElf[0:2]))
+text = (open("day1.txt")).read()
+split_text = text.split("\n\n")
+all = []
+for i in range(len(split_text)):
+    resplit_text = split_text[i].split("\n")
+    all.append(sum([int(x) for x in resplit_text]))
+all.sort(reverse=True)
+print("Part 1:", all[0], " \nPart 2:", sum(all[0:2]))
