bbcace9d674be0069f397e61358fb0c8d201c471
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-12-25 22:23:34 -0800

    FINISHED DAY 7 WOO HOO

--- Day7/ethanday7.py
+++ Day7/ethanday7.py
@@ -1,17 +1,21 @@
 text = open("ethan7.txt").read()
+# text = open("ethan7test.txt").read()
 text = text.split("\n")
-text = text[0:14]
+# text = text[0:30]
 text = [x.split(" ") for x in text]
 
 size_dict = {}
 
-# TODO: Change dictionary keys to be the path to the directory
+def list_sum(key):
+    total_sum = 0
+    for element in size_dict[key]:
+        if type(element) == str:
+            total_sum += list_sum(element)
+        else:
+            total_sum += element
+    return total_sum
 
-def find_sum_of_list(input_list):
-    # TODO: Finish function
-    return
-
-current_path = "/"
+current_path = "~"
 current_dir_index = 0
 while current_dir_index < len(text):
     # print("i: {}".format(current_dir_index))
@@ -19,7 +23,7 @@ while current_dir_index < len(text):
     if line[0] == '$' and line[1] == 'cd' and line[2] != '..':
         current_dir_name = line[2]
         if current_dir_name != '/':
-            current_path += current_dir_name + "/"
+            current_path += '/' + current_dir_name
         current_dir_aggregate_file_size = 0
         size_dict[current_path] = []
         j = current_dir_index + 1
@@ -30,7 +34,7 @@ while current_dir_index < len(text):
                 j += 1
                 continue
             if next_line[0] == 'dir':
-                size_dict[current_path].append(next_line[1])
+                size_dict[current_path].append(current_path + '/' + next_line[1])
                 j += 1
                 continue
             if next_line[0].isnumeric():
@@ -41,9 +45,36 @@ while current_dir_index < len(text):
                 break
         size_dict[current_path].append(current_dir_aggregate_file_size)
         current_dir_index = j
-    else:
-        # TODO: Handle cd .. case
-        
+    if line[2] == '..':
+        split_path = current_path.split('/')
+        current_path = '/'.join(split_path[0:-1])
         current_dir_index += 1
 
-print(size_dict)
+# print(size_dict)
+
+complete_size_dict = {}
+for key in size_dict:
+    complete_size_dict[key] = list_sum(key)
+
+# print(complete_size_dict)
+
+less_than_100k_sum = 0
+
+for key in complete_size_dict:
+    if complete_size_dict[key] <= 100000:
+        # print(key, complete_size_dict[key])
+        less_than_100k_sum += complete_size_dict[key]
+# print(less_than_100k_sum)
+
+current_free_space = 70000000 - complete_size_dict['~']
+space_needed = 30000000 - current_free_space
+print(space_needed)
+
+directory_sizes_that_work = []
+
+for key in complete_size_dict:
+    if complete_size_dict[key] >= space_needed:
+        directory_sizes_that_work.append(complete_size_dict[key])
+        
+directory_sizes_that_work.sort()
+print(directory_sizes_that_work[0])
