73cd16300e30b56e3df4c858ba2edac02116e01a
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-12-28 22:03:39 -0800

    Updated Day 10

--- Day10/ethanday10.py
+++ Day10/ethanday10.py
@@ -1,3 +1,4 @@
+import numpy as np
 text = open("ethan10.txt").read()
 text = text.splitlines()
 
@@ -22,11 +23,34 @@ for i in range(0, len(text)):
 
 # print(cycle_dict)
 
-print(cycle_dict[20] * 20)
-print(cycle_dict[60] * 60)
-print(cycle_dict[100] * 100)
-print(cycle_dict[140] * 140)
-print(cycle_dict[180] * 180)
-print(cycle_dict[220] * 220)
-
 print(cycle_dict[20] * 20 + cycle_dict[60] * 60 + cycle_dict[100] * 100 + cycle_dict[140] * 140 + cycle_dict[180] * 180 + cycle_dict[220] * 220)
+
+cycle_array = np.zeros((6, 40), dtype='int')
+
+print(cycle_array)
+
+def get_cycle_coords(cycle):
+    # if cycle % 40 == 0:
+    #     return (cycle // 40 - 1, 39)
+    return (cycle // 40, (cycle - 1) % 40)
+
+def get_sprite_pixels(cycle):
+    x = cycle_dict[cycle]
+    return (x - 1, x, x + 1)
+
+for cycle in range(1, 241):
+# for cycle in range(1, 3):
+    current_cycle_coords = get_cycle_coords(cycle)
+    current_sprite_pixels = get_sprite_pixels(cycle)
+    if (current_cycle_coords[1]) == current_sprite_pixels[0] or (current_cycle_coords[1]) == current_sprite_pixels[1] or (current_cycle_coords[1]) == current_sprite_pixels[2]:
+        cycle_array[current_cycle_coords[0], current_cycle_coords[1]] = 1
+
+print("New array:")
+print(cycle_array)
+for line in cycle_array:
+    for element in line:
+        if element == 0:
+            print(" ", end=" ")
+        else:
+            print('#', end=" ")
+    print()
